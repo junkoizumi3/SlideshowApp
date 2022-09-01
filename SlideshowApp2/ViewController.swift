124a725edce42b7f35f07f8d25ea4284c2a12199
junkoizumi3 <roseicollis@i.softbank.jp>
2022-09-01 22:34:52 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by 和泉淳子 on 2022/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
